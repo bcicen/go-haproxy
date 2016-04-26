f146f289ae24590eaeef1716d9774aa26baa2fda
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-04-26 15:17:55 -0400

    fix examples

--- client_test.go
+++ client_test.go
@@ -1,18 +1,26 @@
 package haproxy
 
+import (
+	"fmt"
+)
+
 func ExampleHAProxyClient_Stats() {
 	client := &HAProxyClient{
 		Addr: "unix:///var/run/haproxy.sock",
 	}
 	stats, err := client.Stats()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	for _, s := range stats {
 		fmt.Printf("%s: %s\n", s.SvName, s.Status)
 	}
 	// Output:
-	//	static: DOWN
-	//	app1: UP
-	//	app2: UP
-	//	...
+	//static: DOWN
+	//app1: UP
+	//app2: UP
+	//...
 }
 
 func ExampleHAProxyClient_Info() {
@@ -20,21 +28,23 @@ func ExampleHAProxyClient_Info() {
 		Addr: "unix:///var/run/haproxy.sock",
 	}
 	info, err := client.Info()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	fmt.Printf("%s version %s\n", info.Name, info.Version)
 	// Output:
-	//	HAProxy version 1.6.3
+	//HAProxy version 1.6.3
 }
 
 func ExampleHAProxyClient_RunCommand() {
 	client := &HAProxyClient{
 		Addr: "unix:///var/run/haproxy.sock",
 	}
-	result, err := h.RunCommand("show info")
+	result, err := client.RunCommand("show info")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	fmt.Println(result.String())
-	// Output:
-	//	Name: HAProxy
-	//	Version: 1.6.3
-	//	Release_date: 2015/12/25
-	//	Nbproc: 1
-	//	...
 }
