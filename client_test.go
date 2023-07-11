88e856e904530a66289c3b32a05ae3cff5488e41
Vitaliy Ponomarev <v.ponomarev@tinkoff.ru>
2023-07-11 13:08:29 +0300

    build working fork

--- client_test.go
+++ client_test.go
@@ -2,13 +2,18 @@ package haproxy_test
 
 import (
 	"fmt"
+	"github.com/vponomarev/go-haproxy"
+)
 
-	"github.com/bcicen/go-haproxy"
+const (
+	HAPROXY_SOCKET_ADDR = "unix:///var/run/haproxy.sock"
 )
 
 func ExampleHAProxyClient_Stats() {
 	client := &haproxy.HAProxyClient{
-		Addr: "unix:///var/run/haproxy.sock",
+		Addr:      HAPROXY_SOCKET_ADDR,
+		Timeout:   30,
+		TimeoutOp: 5,
 	}
 	stats, err := client.Stats()
 	if err != nil {
@@ -27,7 +32,7 @@ func ExampleHAProxyClient_Stats() {
 
 func ExampleHAProxyClient_Info() {
 	client := &haproxy.HAProxyClient{
-		Addr: "unix:///var/run/haproxy.sock",
+		Addr: HAPROXY_SOCKET_ADDR,
 	}
 	info, err := client.Info()
 	if err != nil {
@@ -36,12 +41,12 @@ func ExampleHAProxyClient_Info() {
 	}
 	fmt.Printf("%s version %s\n", info.Name, info.Version)
 	// Output:
-	//HAProxy version 1.6.3
+	//HAProxy version 2.8.1
 }
 
 func ExampleHAProxyClient_RunCommand() {
 	client := &haproxy.HAProxyClient{
-		Addr: "unix:///var/run/haproxy.sock",
+		Addr: HAPROXY_SOCKET_ADDR,
 	}
 	result, err := client.RunCommand("show info")
 	if err != nil {
